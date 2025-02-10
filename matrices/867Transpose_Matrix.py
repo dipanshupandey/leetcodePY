8cec223d4afacb8534f4253db86fd9605efc5c2e
dipanshupandey <dipanshupandey4488@gmail.com>
2025-02-10 23:49:56 +0530

    solution to leetcode867

---
+++ matrices/867Transpose_Matrix.py
@@ -0,0 +1,4 @@
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        return [[matrix[row][col] for row in range(len(matrix))] for col in range(len(matrix[0]))]
+
